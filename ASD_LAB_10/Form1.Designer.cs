fd75bfc684f192c1417a18296a7667abb7015aeb
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-14 20:11:20 +0200

    Add project files.

---
+++ ASD_LAB_10/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ASD_LAB_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.pattern = new System.Windows.Forms.TextBox();
+            this.target = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(112, 215);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(167, 37);
+            this.start.TabIndex = 1;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // pattern
+            // 
+            this.pattern.Location = new System.Drawing.Point(179, 100);
+            this.pattern.Name = "pattern";
+            this.pattern.Size = new System.Drawing.Size(100, 20);
+            this.pattern.TabIndex = 2;
+            // 
+            // target
+            // 
+            this.target.Location = new System.Drawing.Point(179, 153);
+            this.target.Name = "target";
+            this.target.Size = new System.Drawing.Size(100, 20);
+            this.target.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PATTERN : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "TARGET : ";
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.result.Location = new System.Drawing.Point(351, 100);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(171, 151);
+            this.result.TabIndex = 6;
+            this.result.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.target);
+            this.Controls.Add(this.pattern);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.TextBox pattern;
+        private System.Windows.Forms.TextBox target;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox result;
+    }
+}
+
---
+++ ASD_LAB_10/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASD_LAB_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void clear()
+        {
+            result.Text = "";
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            clear();
+            string pt = pattern.Text;
+            string tg = target.Text;
+
+            for (int i = 0; i < tg.Length - pt.Length + 1; i++)
+            {
+                int j = i;
+                bool check = false;
+                string temp = pt;
+                foreach(char ch in pt)
+                {
+                    
+                    if (ch == '?')
+                    {
+                        temp = temp.Replace('?', tg[j]);
+                    }
+                    else 
+                    {
+                        if (ch == tg[j])
+                        {
+                            check = true;
+                        }
+                        else
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+                    
+                    j++;
+                }
+                if (check)
+                {
+                    result.Text += $"\nINDEX ({i}) : {temp}";
+                }
+            }
+        }
+    }
+}
